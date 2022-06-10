06aeb215dc26ab6fb96d39549c2b3a149b830fcf
MishalPopy <c.mishal46@gmail.com>
2022-06-10 14:57:36 +0530

    first142 commit

--- learn_app/settings.py
+++ learn_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w=dtl!9^h7baq^(qo&g^q)a&4+ez#$!&7tildc(i%srwrj3p*^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition 
